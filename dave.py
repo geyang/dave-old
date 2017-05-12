25f01f2219b4c6fd32765bb68f58ff6a20bb8c63
Ge Yang <yangge1987@gmail.com>
2017-05-11 19:53:50 -0500

    add command scafold

--- dave.py
+++ dave.py
@@ -4,8 +4,19 @@ import click
 
 @click.command()
 @click.option('-c', default='./dave.yml', help='location of the configuration file for the experiment')
-def main(*args, **kwargs):
+def main(c, *args, **kwargs):
     """the dave main script for helping you with experiments."""
-    click.echo(args)
-    click.echo(kwargs)
-    click.echo('hello World!!')
+    click.echo('using configuration file: '.format(c))
+
+@click.command()
+@click.option('-c', default='./dave.yml', help='location of the configuration file for the experiment')
+def add(c, *args, **kwargs):
+    """the dave main script for helping you with experiments."""
+    click.echo('add configuration to list of jobs'.format(c))
+
+
+@click.command()
+def list(c, *args, **kwargs):
+    """the dave main script for helping you with experiments."""
+    click.echo('list jobs')
+
